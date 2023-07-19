626984cf79f4df04e1bef4ed7a7037a10c3515bc
dnaielrfranco <danielfranco9132@gmail.com>
2023-07-19 15:32:01 -0600

    Task #2

--- src/Grid.tsx
+++ src/Grid.tsx
@@ -5,16 +5,16 @@ import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-alpine.css";
 
 const columnDefs: ColDef[] = [
-  { field: "designation", headerName: "Designation" },
-  { field: "discovery_date", headerName: "Discovery Date" },
-  { field: "h_mag", headerName: "H (mag)" },
-  { field: "moid_au", headerName: "MOID (au)" },
-  { field: "q_au_1", headerName: "q (au)" },
-  { field: "q_au_2", headerName: "Q (au)" },
-  { field: "period_yr", headerName: "Period (yr)" },
-  { field: "i_deg", headerName: "Inclination (deg)" },
-  { field: "pha", headerName: "Potentially Hazardous" },
-  { field: "orbit_class", headerName: "Orbit Class", enableRowGroup: true, },
+  { field: "designation", headerName: "Designation", sortable: true, filter: true},
+  { field: "discovery_date", headerName: "Discovery Date", sortable: true, filter: true},
+  { field: "h_mag", headerName: "H (mag)", sortable: true, filter: true},
+  { field: "moid_au", headerName: "MOID (au)", sortable: true, filter: true},
+  { field: "q_au_1", headerName: "q (au)", sortable: true, filter: true},
+  { field: "q_au_2", headerName: "Q (au)", sortable: true, filter: true},
+  { field: "period_yr", headerName: "Period (yr)", sortable: true, filter: true},
+  { field: "i_deg", headerName: "Inclination (deg)", sortable: true, filter: true},
+  { field: "pha", headerName: "Potentially Hazardous", sortable: true, filter: true},
+  { field: "orbit_class", headerName: "Orbit Class", enableRowGroup: true, sortable: true, filter: true},
 ];
 
 const NeoGrid = (): JSX.Element => {
